3a9e2b7094e6682ca30eeae5819fcd4ae74a100f
Timo De Waele <timo.dewaele@ugent.be>
2022-12-01 09:22:51 +0100

    Day 1

---
+++ day_1/main.rs
@@ -0,0 +1,34 @@
+use std::fs;
+
+fn main() {
+    let input_file_path = "full.input";
+    let contents = fs::read_to_string(input_file_path).unwrap();
+    println!("Part 1: {}", part_1(&contents));
+    println!("Part 2: {}", part_2(&contents));
+}
+
+fn part_1(list: &str) -> i64 {
+    list.split("\n\n").map(|elf_list| {
+        elf_list.split("\n")
+        .map(|x| {
+            x.parse::<i64>().unwrap()
+        })
+        .sum::<i64>()
+    })
+    .max()
+    .unwrap()
+}
+
+fn part_2(list: &str) -> i64 {
+    let mut x: Vec<i64> = list.split("\n\n").map(|elf_list| {
+        elf_list.split("\n")
+        .map(|x| {
+            x.parse::<i64>().unwrap()
+        })
+        .sum::<i64>()
+    })
+    .collect::<Vec<i64>>();
+    x.sort_by(|a, b| b.cmp(a));
+    let top_three: &[i64] = &x[..3];
+    top_three.iter().sum::<i64>()
+}
--- template/main.rs
+++ template/main.rs
@@ -1,2 +1,7 @@
+use std::fs;
+
 fn main() {
+    let input_file_path = "test.input";
+    let contents = fs::read_to_string(input_file_path).unwrap();
+    println!("{}", contents);
 }
